72e088d87efe19dd0bcf4f263f6189e9e6a1bc2a
Erik Culberson <eculberson12@gmail.com>
2015-04-16 18:26:35 -0400

    Code Structire

--- app/src/main/java/com/ultimatumedia/erik/taskmanager/MainActivity.java
+++ app/src/main/java/com/ultimatumedia/erik/taskmanager/MainActivity.java
@@ -1,5 +1,7 @@
 package com.ultimatumedia.erik.taskmanager;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.app.TimePickerDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -19,88 +21,10 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button timeNeededToWakeUp = (Button)findViewById(R.id.TimeNeededToWakeUp);
-        final Button hoursNeededToSleep = (Button)findViewById(R.id.HoursNeededToSleep);
-        final TextView timeToGoToSleep = (TextView)findViewById(R.id.TimeToGoToSleep);
-
-        timeNeededToWakeUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String[] time = timeNeededToWakeUp.getText().toString().split(":");
-                int hour = Integer.parseInt(time[0]);
-                int minute = Integer.parseInt(time[1]);
-                TimePickerDialog tpd = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
-
-                            @Override
-                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                                //timeNeededToWakeUp.setText(hourOfDay + ":" + sMinute);
-                                //timeToGoToSleep.setText(hourTimeToSleep + ":" + sMinute + ap);
-                            }
-                        }, hour, minute, false);
-                tpd.setTitle("Set Time Needed To Wake Up");
-                tpd.show();
-            }
-        });
-
-        hoursNeededToSleep.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String[] time = hoursNeededToSleep.getText().toString().split(":");
-                int hour = Integer.parseInt(time[0]);
-                int minute = Integer.parseInt(time[1]);
-                TimePickerDialog tpd = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
-
-                            @Override
-                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                                //hoursNeededToSleep.setText(hourOfDay + ":" + sMinute);
-                                //timeToGoToSleep.setText(FormatTime(true, hourOfDay, minute);
-                            }
-                        }, hour, minute, true);
-                tpd.setTitle("Set Time Needed To Sleep");
-                tpd.show();
-            }
-        });
-
+        Fragment sleepCalculatorFragment = new SleepCalculatorFragment();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.add(R.id.FragmentContainer, sleepCalculatorFragment).commit();
     }
-
-    public String FormatTime(Boolean is24Hour, int hour, int minute) {
-        String sHour = Integer.toString(hour);
-        String sMinute = Integer.toString(minute);
-        String ap = "";
-
-        if(minute < 10){
-            sMinute = "0" + minute;
-        }
-        if(is24Hour) {
-            if (hour == 0) {
-                hour = 12;
-                sHour = Integer.toString(hour);
-                ap = "AM";
-            }
-            if (hour > 12) {
-                hour -= 12;
-                sHour = Integer.toString(hour);
-                ap = "PM";
-            }
-        }
-        return sHour + ":" + sMinute + " " + ap;
-    }
-
-    private String CalculateTimeToSleep(int hourSleepNeeded, int minuteSleepNeeded, int hourToWakeUp, int minuteToWakeUp) {
-        int hourTimeToSleep = hourSleepNeeded - hourToWakeUp;
-        int minuteTimeToSleep = minuteSleepNeeded - minuteToWakeUp;
-        String ap = "";
-
-        if(hourTimeToSleep < 0) {
-            hourTimeToSleep += 24;
-        }
-
-        if(minuteTimeToSleep < 0) {
-            minuteTimeToSleep += 60;
-        }
-        return hourTimeToSleep + ":" + minuteTimeToSleep + " " + ap;
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
